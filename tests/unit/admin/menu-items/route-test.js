0051fe528e94f20be2394a1818e87a788aa8a88b
David Cawton II <Dvaidc@Davids-MacBook-Pro.local>
2015-12-30 14:26:45 -0600

    add additional models

---
+++ tests/unit/admin/menu-items/route-test.js
@@ -0,0 +1,11 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('route:admin/menu-items', 'Unit | Route | admin/menu items', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+test('it exists', function(assert) {
+  let route = this.subject();
+  assert.ok(route);
+});
