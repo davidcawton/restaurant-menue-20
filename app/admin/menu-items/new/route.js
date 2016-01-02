e21053c5111d50581d41e1849d7dbfd4fffdbb1e
David Cawton II <Dvaidc@Davids-MacBook-Pro.local>
2016-01-02 14:12:16 -0600

    Added addMenuItem action

--- app/admin/menu-items/new/route.js
+++ app/admin/menu-items/new/route.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  actions: {
+    addMenuItem(newItem) {
+      let item = this.store.createRecord('newItem', newItem);
+      addMenuItem.save().then(() => {
+        this.transitionTo('admin');
+      });
+    },
+  },
 });
