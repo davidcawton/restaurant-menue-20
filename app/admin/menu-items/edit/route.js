b17b63699d1ada44f7d71cee18fb553baaf40ce5
David Cawton II <Dvaidc@Davids-MacBook-Pro.local>
2016-01-03 11:44:24 -0600

    Trying To Delete menu Item

--- app/admin/menu-items/edit/route.js
+++ app/admin/menu-items/edit/route.js
@@ -6,6 +6,14 @@ export default Ember.Route.extend({
   },
 
   actions: {
+    removeItem(formData) {
+      let removeItem = this.get('admin.menu-items.edit');
+      removeItem.deleteRecord(formData);
+      removeItem.save().then(() => {
+        this.transitionTo('admin.menu-items');
+      });
+    },
+
     saveItem(formData) {
       let menuItem = this.modelFor('admin.menu-items.edit');
       menuItem.setProperties(formData);
